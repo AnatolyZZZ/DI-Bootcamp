d4c19ff1a1c7b727b7279fa16f7ca81d2089a2e9
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-02-09 13:29:53 +0200

    Daly challange done W4D5

--- Week4/Day5/DailyChallange/script.js
+++ Week4/Day5/DailyChallange/script.js
@@ -1,3 +1,21 @@
 const checkForAnagram = (str1, str2) => {
-
+    arr1 = str1.toUpperCase().split("");
+    arr2 = str2.toUpperCase().split("");
+    isAnn = true;
+    const delSpace = (arr) => arr.filter(elt => elt != " " && elt !="\xa0");
+    arr1 = delSpace(arr1).sort();
+    arr2 = delSpace(arr2).sort();
+    console.log(arr1)
+    
+    // debugger;
+    if (arr1.length != arr2.length) {isAnn = false} else {
+        for (let i = 0; i<arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {isAnn = false};
+        }
+    }
+    return isAnn;
 }
+
+console.log(checkForAnagram("Astronomer", "Moon starer"));
+console.log(checkForAnagram("School    master", "The classroom"));
+console.log(checkForAnagram("The MorseCode", "Here come dots"));
