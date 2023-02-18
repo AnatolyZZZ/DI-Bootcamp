6183fe1f7b1902eceb9ba2c1936ce8b30fa3741f
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-02-18 16:01:24 +0200

    Pokemon project finalize

--- Week5/Day5/PokemonMiniproject/script.js
+++ Week5/Day5/PokemonMiniproject/script.js
@@ -5,6 +5,16 @@ const foto = document.querySelector("#foto");
 const spawn = document.querySelector("#green");
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
+errorDisplayed = false;
+const errorP = document.createElement('p');
+errorP.appendChild(document.createTextNode("Sorry, could't catch new Pokemon :-("))
+infoDiv.appendChild(errorP);
+errorP.classList.add("invisible");
+const searchImg = document.createElement('img');
+searchImg.setAttribute("src", "loading.gif");
+searchImg.classList.add("poke-image");
+foto.appendChild(searchImg);
+searchImg.classList.add("invisible");
 
 left.addEventListener("click", moveLeft);
 right.addEventListener("click", moveRight);
@@ -45,11 +55,28 @@ function displayPokemon (info) {
 
 }
 
-async function animateRight (obj) {
+async function animateRight (obj, par) {
     const p = new Promise(function (resolve, reject) {
     let i = 0;
     function stepRight () {
-        obj.style.left = `${i*5}px`;
+        obj.style.left = `${par+i*5}px`;
+        i++;
+        if (i === 35) {
+            clearInterval(id);
+            obj.style.left = `50px`;
+            resolve("moved");
+        }
+    }
+    const id = setInterval (stepRight, 1);
+})
+    return p
+}
+
+async function animateLeft (obj, par) {
+    const p = new Promise(function (resolve, reject) {
+    let i = 0;
+    function stepRight () {
+        obj.style.left = `${par-i*5}px`;
         i++;
         if (i === 35) {
             clearInterval(id);
@@ -65,10 +92,13 @@ async function animateRight (obj) {
 async function moveRight() {
     // debugger;
     if (current === arrObj.length - 1) {return}
+    arrObj[current+1][0].classList.remove("invisible");
+    arrObj[current+1][0].style.left = "225px";
+    prom = Promise.all([animateLeft (arrObj[current+1][0], 225), animateLeft (arrObj[current][0], 0)]);
+    await prom;
     arrObj[current][0].classList.add("invisible");
     arrObj[current][1].classList.add("invisible");
     current++;
-    arrObj[current][0].classList.remove("invisible");
     arrObj[current][1].classList.remove("invisible");
 };
 
@@ -76,8 +106,8 @@ async function moveLeft() {
     if (current === 0) {return}
     arrObj[current-1][0].classList.remove("invisible");
     arrObj[current-1][0].style.left = "-175px";
-    await animateRight (arrObj[current-1][0])
-    await animateRight (arrObj[current][0]);
+    prom = Promise.all([animateRight (arrObj[current-1][0], -175), animateRight (arrObj[current][0], 0)])
+    await prom;
     arrObj[current][0].classList.add("invisible");
     arrObj[current][1].classList.add("invisible");
     current--;
@@ -85,19 +115,60 @@ async function moveLeft() {
 };
 
 
-
-async function getPokemon () {
-    const poke = await fetchOne("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
-    const num = Math.floor(Math.random() * 1279);
-    const pokeObj = poke.results[num];
-    ({name ,url} = pokeObj)
-    // console.log(name);
-    const onePokemon = await fetchOne(url);
-    ({height, weight, id, types, sprites} = onePokemon)
-    const image = sprites.front_default;
-    const type = types[0].type.name;
-    const pokemonInfo = [name, height, weight, id, type, image]
-    displayPokemon(pokemonInfo);
+function undisplayError () {
+    errorDisplayed = false;
+    errorP.classList.add("invisible");
+    if (arrObj.length >0) {
+        arrObj[current][1].classList.remove("invisible");
+        arrObj[current][1].classList.remove("invisible");
+    }
 }
 
-getPokemon();
+
+function displayError () {
+    errorDisplayed = true;
+    errorP.classList.remove("invisible");
+    if (arrObj.length >0) {
+        arrObj[current][1].classList.add("invisible");
+        arrObj[current][1].classList.add("invisible");
+    }
+}
+
+function displaySearch () {
+    searchImg.classList.remove("invisible");
+    if (arrObj.length >0) {
+        arrObj[current][0].classList.add("invisible");
+        arrObj[current][0].classList.add("invisible");
+    }
+}
+
+function undisplaySearch () {
+    searchImg.classList.add("invisible");
+    if (arrObj.length >0) {
+        arrObj[current][0].classList.remove("invisible");
+        arrObj[current][0].classList.remove("invisible");
+    }
+}
+
+
+async function getPokemon () {
+    try {
+        displaySearch();
+        const poke = await fetchOne("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+        const num = Math.floor(Math.random() * 1279);
+        const pokeObj = poke.results[num];
+        ({name ,url} = pokeObj)
+        // console.log(name);
+        const onePokemon = await fetchOne(url);
+        ({height, weight, id, types, sprites} = onePokemon)
+        const image = sprites.front_default;
+        const type = types[0].type.name;
+        const pokemonInfo = [name, height, weight, id, type, image];
+        undisplaySearch();
+        displayPokemon(pokemonInfo);
+    } catch {
+        displayError();
+        setTimeout(undisplayError, 3000);
+        undisplaySearch();
+    }
+}
