3e506ce90f60f45480053e36443f56890b81579b
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-02-21 16:54:12 +0200

    W5D2 XPGold in process

--- Week5/Day2/XPGold/script.js
+++ Week5/Day2/XPGold/script.js
@@ -1,13 +1,75 @@
-const htr = new XMLHttpRequest;
 const main = document.querySelector("#main")
-htr.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
-htr.send();
-
-htr.onload = function () {
-    obj = JSON.parse(htr.response);
-    const url = obj.data.url + "?api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"
-    console.log(url);
-    const img = document.createElement("embed");
-    img.setAttribute("src", url);
-    main.appendChild(img);
+function fetchOne () {
+    return new Promise (function (resolve, reject) {
+        const htr = new XMLHttpRequest();
+        try {
+            htr.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My");
+            htr.send();
+        } catch (e) {
+            console.log("in first catch")
+            console.log(e.message);
+            reject(e.message);
+        }
+       
+        htr.onload = function () {
+        const obj = JSON.parse(htr.response);
+        if (obj.meta.status != 200) {
+            console.log(obj.meta.status);
+            reject("Could't fetch");
+        } else {
+            const url = obj.data.images.downsized_medium.url;
+            resolve(url);
+        }
+        }
+    })
 }
+
+function addToPage (url) {
+    const image = document.createElement('img');
+    image.setAttribute('src', url);
+    image.setAttribute('alt', "Funny GIF");
+    main.appendChild(image);
+    return image;
+}
+
+function displayMessege (msg) {
+    p = document.createElement("p");
+    p.appendChild(document.createTextNode(msg));
+    p.style.fontSize = "x-large";
+    p.style.textTransform = "uppercase";
+    main.appendChild(p);
+    return p;
+}
+
+async function doTheJob () {
+    const dl = displayMessege("downloading")
+    try {
+        const url = await fetchOne();
+        dl.remove();
+        addToPage(url);
+    } catch (e) {
+        dl.remove();
+        displayNessege(e.message);
+    }
+    // 
+    // const pr = fetchOne()
+    // pr.then (
+    //    function (url) {
+    //     console.log(pr);
+    //     addToPage(url);
+    //     dl.remove();
+    //    }
+    // )
+    // .catch (function (msg) {
+    //     console.log(pr);
+    //     console.log("in second catch")
+    //     dl.remove();
+    //     displayNessege(msg);
+    // }   
+    // )
+    
+}
+
+doTheJob ();
+
+// console.log(fetchOne());
