ac75a630d371384c7676716caf886748b7cd4e1c
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-04-27 11:00:50 +0300

    fp header, logout

--- FinalProject/server/routes/Users.js
+++ FinalProject/server/routes/Users.js
@@ -1,4 +1,4 @@
-import  {_newUser, _changeLevel, login, getToken} from '../controllers/Users.js'
+import  {_newUser, _changeLevel, login, getToken, logout} from '../controllers/Users.js'
 import express from 'express'
 import { VerifyToken } from '../middlwares/VerifyToken.js';
 
@@ -7,5 +7,6 @@ export const user_router = express.Router();
 user_router.post('/register', _newUser);
 user_router.put('/lvl', VerifyToken, _changeLevel);
 user_router.post('/login', login);
+user_router.get('/logout', logout);
 user_router.get('/token', VerifyToken, getToken)
 
